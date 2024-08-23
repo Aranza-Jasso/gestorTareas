6b1375a59d179ea0e6546954a82bcb366a649982
Aranza Jasso <aranza.educativo12@gmail.com>
2024-08-22 18:02:38 -0600

    proyecto terminado

--- src/task.js
+++ src/task.js
@@ -1,11 +1,33 @@
-// Obtener la lista de tareas del almacenamiento del navegador
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-// Función para agregar una tarea a la lista de tareas
-export const addTask =  (task) => {
+// Lista de tareas
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+ 
+// Función para llevar las tareas
+export const getTasks = () => tasks;
+ 
+// Función para agregar una tarea
+export const addTask = (task) => {
     const newTask = {
         id: Date.now(),
         text: task,
         completed: false,
     };
-}
+    tasks.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+ 
+// Función para eliminar una tarea
+export const deleteTask = (id) => {
+    tasks = tasks.filter((task) => task.id !== parseInt(id));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+ 
+// Función para actualizar una tarea
+export const toggleTask = (id) => {
+    tasks = tasks.map((task) => {
+        if(task.id === parseInt(id)) {
+            task.completed = !task.completed;
+        }
+        return task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
-
+ 
 module.exports = {
-    entry: './src/index.js', // Punto de entrada de tu aplicación
+    mode: 'development',
+    entry: './src/index.js', // Punto de entrada de la applicación
     output: {
         filename: 'bundle.js', // Nombre del archivo de salida
         path: path.resolve(__dirname, 'dist'), // Carpeta de salida
@@ -9,25 +10,27 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/, // Regex para identificar archivoas css
-                use: ['style-loader', 'css-loader'], // Loaders para procesar archivos
+                test: /\.css$/, // Regex para identificar archivos CSS
+                use: ['style-loader', 'css-loader'], // Loaders para procesar archivos CSS
             },
             {
                 test: /\.js$/, // Regex para identificar archivos JS
-                exclude: /node_modules/, // Excluir carpeta modules
+                exclude: /node_modules/, // Excluir la carpeta node_modules
                 use: {
-                    loader: 'babel-loader', // Loader para transpilar Js moderno a JS compatible con más navegadores
+                    loader: 'babel-loader', // Loader para llevar JS moderno a JS antiguo para que sea compatible con todos los navegadores
                     options: {
-                        presets: ['@babel/preset-env'], // Preset de babel para convertir JS moderno a versiones más antiguas
-                    }
-                }
-            }
-        ]
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
     },
     devtool: 'source-map', // Generar source maps para facilitar la depuración
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist'), // Carpeta desde donde optenemos los archivos para mostrarlos al usuario
-        compress: true, // Habilitando compresión gzip
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        compress: true, // Habilitar compresión gzip
         port: 9000, // Puerto del servidor de desarrollo
-    }
-}
+    },
+};
